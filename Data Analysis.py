a814e94b51fab60d6b158f2decc4b1639940792b
Frank Pasqualini <FJP11@pitt.edu>
2019-03-20 00:07:43 -0400

    Histograms and scatter plots

--- Data Analysis.py
+++ Data Analysis.py
@@ -1,5 +1,46 @@
 import pickle
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def gather(data, category):
+    output = []
+    if category[0:3] == "adj":
+        counter = 0
+        for item in data:
+            while counter < int(item['playcount']):
+                if category == 'name' or category == 'artist' or category == 'album':
+                    output.append(item[category[4:]])
+                else:
+                    output.append(float(item[category[4:]]))
+                counter = counter + 1
+    else:
+        for item in data:
+            if category == 'name' or category == 'artist' or category == 'album':
+                output.append(item[category])
+            else:
+                output.append(float(item[category]))
+    return output
+
+
+def plot_hist(data, title):
+    if title == 'name' or title == 'artist' or title == 'album':
+        return
+
+    plt.hist(x=data, bins='auto')
+    plt.title(title + " (x̄=" + str(np.mean(data)) + " s=" + str(np.std(data)) + ")")
+    plt.show()
+
+
+def plot_scatter(data, title, playcount):
+    if title == 'name' or title == 'artist' or title == 'album' or title[0:3] == "adj":
+        return
+
+    plt.plot(data, playcount, 'o')
+    plt.title("playcount vs " + title)
+    plt.show()
+
 
 def main():
     # Reads data from the file
@@ -7,8 +48,18 @@ def main():
     data = pickle.load(out_file)
     out_file.close()
 
-    for item in data:
-        print(item)  # TODO Analyze Data
+    category_lists = {'name': [], 'artist': [], 'album': [], 'playcount': [], 'duration_ms': [], 'explicit': [],
+                      'popularity': [], 'key': [], 'mode': [], 'time_signature': [], 'acousticness': [],
+                      'danceability': [], 'energy': [], 'instrumentalness': [], 'liveness': [], 'loudness': [],
+                      'speechiness': [], 'valence': [], 'tempo': [], 'adj_duration_ms': [], 'adj_explicit': [],
+                      'adj_popularity': [], 'adj_key': [], 'adj_mode': [], 'adj_time_signature': [],
+                      'adj_acousticness': [], 'adj_danceability': [], 'adj_energy': [], 'adj_instrumentalness': [],
+                      'adj_liveness': [], 'adj_loudness': [], 'adj_speechiness': [], 'adj_valence': [], 'adj_tempo': []}
+
+    for category in category_lists:
+        category_lists[category] = gather(data, category)
+        plot_hist(category_lists[category], category)
+        plot_scatter(category_lists[category], category, category_lists['playcount'])
 
 
 if __name__ == "__main__":
